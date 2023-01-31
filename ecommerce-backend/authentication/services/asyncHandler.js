8e70260bff2bed1787a57d651ae3985cd7084911
sidp919 <sidp0008@gmail.com>
2023-01-31 20:46:48 +0530

    Backend:Auth:07(correction)- updated asyncHandler function to handle asynchronous operations

--- ecommerce-backend/authentication/services/asyncHandler.js
+++ ecommerce-backend/authentication/services/asyncHandler.js
@@ -2,11 +2,12 @@ const asyncHandler = (func) => async (req, res, next) => {
     try {
         await func(req, res, next)
     } catch (error) {
-        res.status(error.code).json({
+        res.status(error.code || 500).json({
             success: false,
             message: error.message,
+            code:error.code
         })
     }
 }
 
-export default asyncHandler;
+module.exports = asyncHandler;
